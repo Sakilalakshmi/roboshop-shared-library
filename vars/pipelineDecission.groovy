9af6d2465c99b4826a122463ff15606f66773f1a
Sakilalakshmi <sakilalakshmi95@gmail.com>
2023-11-10 18:52:39 +0530

    added shared library

--- vars/pipelineDecission.groovy
+++ vars/pipelineDecission.groovy
@@ -1,5 +1,19 @@
 #! groovy
 
 def decidePipleine(Map configMap){
-    echo "I need to take decision based on the map you sent"
+    application = configMap.get("application")
+    //# here we are getting nodeJSVM
+    switch(application) {
+        case 'nodeJSVM':
+            echo "application is nodeJS and VM based"
+            //nodeJSVMCI(configMap)
+            break
+        case 'javaVM':
+            javaVMCI(configMap)
+            break
+        default:
+            error "Un recognised application"
+            break
+    }
+    //echo "I need to take decision based on the map you sent"
 }
